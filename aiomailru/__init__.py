13b0e8455ff08c8c92ec429fb6789cee52d2e7ce
KonstantinTogoi <konstantin.togoi@gmail.com>
2019-06-28 11:36:30 +0300

    add APIScrapperError

--- aiomailru/__init__.py
+++ aiomailru/__init__.py
@@ -1,7 +1,13 @@
 from . import api, exceptions, objects, parser, sessions, utils
 
 from .utils import parseaddr
-from .exceptions import Error, AuthorizationError, APIError
+from .exceptions import (
+    Error,
+    AuthorizationError,
+    APIError,
+    APIScrapperError,
+    CookieError,
+)
 from .sessions import (
     PublicSession,
     ClientSession,
