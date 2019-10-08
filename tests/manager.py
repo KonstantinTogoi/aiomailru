1dc71458e0cc68a8bc1698864f4d38b40bebedca
Konstantin Togoi <konstantin.togoi@gmail.com>
2019-10-08 12:10:12 +0300

    refactor test data manager

--- tests/manager.py
+++ tests/manager.py
@@ -7,117 +7,136 @@ from aiomailru import API
 from aiomailru.sessions import TokenSession, ImplicitSession
 
 
-test_case_name = os.environ.get('AIOMAILRU_TEST_CASE_NAME', 'example')
-accounts_path = f'data/accounts/{test_case_name}.json'
-applications_path = f'data/applications/{test_case_name}.json'
-cookies_path = f'data/cookies/{test_case_name}.json'
-tokens_path = f'data/tokens/{test_case_name}.json'
+TEST_CASE_NAME = os.environ.get('AIOMAILRU_TEST_CASE_NAME', 'example')
 
 
-def update_account(user_acc_name, uid):
-    """Updates account uid."""
+class Manager:
+    """Test data manager."""
 
-    with open(accounts_path) as f:
-        accounts = json.load(f)
+    @property
+    def accounts(self):
+        with open(self.accounts_path) as f:
+            return json.load(f)
 
-    accounts[user_acc_name]['uid'] = uid
+    @accounts.setter
+    def accounts(self, value):
+        with open(self.accounts_path, 'w') as f:
+            json.dump(value, f, indent=2)
 
-    with open(accounts_path, 'w') as fw:
-        json.dump(accounts, fw, indent=2)
+    @property
+    def applications(self):
+        with open(self.applications_path) as f:
+            return json.load(f)
+
+    @applications.setter
+    def applications(self, value):
+        with open(self.applications_path, 'w') as f:
+            json.dump(value, f, indent=2)
+
+    @property
+    def cookies(self):
+        with open(self.cookies_path) as f:
+            return json.load(f)
+
+    @cookies.setter
+    def cookies(self, value):
+        with open(self.cookies_path, 'w') as f:
+            json.dump(value, f, indent=2)
+
+    @property
+    def tokens(self):
+        with open(self.tokens_path) as f:
+            return json.load(f)
+
+    @tokens.setter
+    def tokens(self, value):
+        with open(self.tokens_path, 'w') as f:
+            json.dump(value, f, indent=2)
+
+    def __init__(self, test_case_name=TEST_CASE_NAME):
+        self.accounts_path = f'data/accounts/{test_case_name}.json'
+        self.applications_path = f'data/applications/{test_case_name}.json'
+        self.cookies_path = f'data/cookies/{test_case_name}.json'
+        self.tokens_path = f'data/tokens/{test_case_name}.json'
+
+    def update_account(self, user_acc_name, uid):
+        """Updates account uid."""
+        accounts = self.accounts
+        accounts[user_acc_name]['uid'] = uid
+        self.accounts = accounts
+
+    def update_cookies(self, user_acc_name, app_name, user_cookies):
+        """Updates user cookies."""
+        cookies = self.cookies
+        if user_acc_name not in cookies:
+            cookies[user_acc_name] = {}
+        cookies[user_acc_name][app_name] = user_cookies
+        self.cookies = cookies
+
+    def update_token(self, user_acc_name, app_name, user_token):
+        """Updates user token."""
+        tokens = self.tokens
+        if user_acc_name not in tokens:
+            tokens[user_acc_name] = {}
+        tokens[user_acc_name][app_name] = user_token
+        self.tokens = tokens
 
 
-def update_cookies(user_acc_name, app_id, user_cookies):
-    """Updates user cookies."""
+async def authorize(user_acc_name, admin_acc_name, app_name, app_id=0):
+    print('authorize', user_acc_name, 'at', app_name)
 
-    with open(cookies_path) as fr:
-        cookies = json.load(fr)
+    manager = Manager()
+    accounts = manager.accounts
+    applications = manager.applications
 
-    if user_acc_name not in cookies:
-        cookies[user_acc_name] = {}
-
-    cookies[user_acc_name][app_id] = user_cookies
-
-    with open(cookies_path, 'w') as fw:
-        json.dump(cookies, fw, indent=2)
-
-
-def update_token(user_acc_name, app_id, user_token):
-    """Updates user token."""
-
-    with open(tokens_path, 'r') as fr:
-        tokens = json.load(fr)
-
-    if user_acc_name not in tokens:
-        tokens[user_acc_name] = {}
-
-    tokens[user_acc_name][app_id] = user_token
-
-    with open(tokens_path, 'w') as fw:
-        json.dump(tokens, fw, indent=2)
-
-
-async def authorize(user_acc_name, admin_acc_name, app_id):
-    print('authorize', user_acc_name, 'at', app_id)
-
-    with open(accounts_path) as f:
-        accounts = json.load(f)
-    with open(applications_path) as f:
-        applications = json.load(f)
-
+    # account must be registered in test data
     acc_info = accounts[user_acc_name]
-    app_info = applications.get(admin_acc_name, {
-        str(app_id): {
-            'app_id': int(app_id),
+    # if application is new
+    if admin_acc_name not in applications:
+        applications[admin_acc_name] = {app_name: {
+            'app_id': app_id,
+            'private_key': '',
             'secret_key': '',
-            'scope': 'stream messages'
-        }
-    })[app_id]
+            'scope': '',
+        }}
+        manager.applications = applications
+    app_info = applications[admin_acc_name][app_name]
 
+    # authorize
     s = await ImplicitSession(**acc_info, **app_info, pass_error=True)
-    update_account(user_acc_name, s.uid)
-    update_cookies(user_acc_name, app_id, s.cookies)
-    update_token(user_acc_name, app_id, s.session_key)
+    manager.update_account(user_acc_name, s.uid)
+    manager.update_cookies(user_acc_name, app_name, s.cookies)
+    manager.update_token(user_acc_name, app_name, s.session_key)
     await s.close()
 
 
-async def login(user_acc_name, admin_acc_name, app_id):
-    print('login', user_acc_name, 'at', app_id)
+async def login(user_acc_name, admin_acc_name, app_name, app_id=0):
+    print('login', user_acc_name, 'at', app_name)
 
-    with open(accounts_path) as f:
-        accounts = json.load(f)
-    with open(applications_path) as f:
-        applications = json.load(f)
-    with open(cookies_path) as f:
-        cookies = json.load(f)
-    with open(tokens_path) as f:
-        tokens = json.load(f)
+    manager = Manager()
+    cookies = manager.cookies
+    tokens = manager.tokens
 
     # if account wasn't authorized yet at all
     if user_acc_name not in cookies or user_acc_name not in tokens:
-        await authorize(user_acc_name, admin_acc_name, app_id)
-        with open(cookies_path, 'r') as fr:
-            cookies = json.load(fr)
-        with open(tokens_path, 'r') as fr:
-            tokens = json.load(fr)
+        await authorize(user_acc_name, admin_acc_name, app_name, app_id)
+        cookies = manager.cookies
+        tokens = manager.cookies
 
     # if account wasn't authorized at the app
-    elif app_id not in cookies[user_acc_name] or \
-            app_id not in tokens[user_acc_name]:
-        await authorize(user_acc_name, admin_acc_name, app_id)
-        with open(cookies_path, 'r') as fr:
-            cookies = json.load(fr)
-        with open(tokens_path, 'r') as fr:
-            tokens = json.load(fr)
+    elif app_name not in cookies[user_acc_name] or \
+            app_name not in tokens[user_acc_name]:
+        await authorize(user_acc_name, admin_acc_name, app_name, app_id)
+        cookies = manager.cookies
+        tokens = manager.cookies
+
+    accounts = manager.accounts
+    applications = manager.applications
 
     acc_info = accounts[user_acc_name]
-    app_info = applications.get(admin_acc_name, {
-        str(app_id): {
-            'app_id': int(app_id),
-            'secret_key': '',
-        }
-    })[app_id]
+    app_info = applications[admin_acc_name][app_name]
     token_info = {
-        'access_token': tokens[user_acc_name][app_id],
-        'cookies': cookies[user_acc_name][app_id],
+        'access_token': tokens[user_acc_name][app_name],
+        'cookies': cookies[user_acc_name][app_name],
     }
     return TokenSession(**acc_info, **app_info, **token_info, pass_error=True)
